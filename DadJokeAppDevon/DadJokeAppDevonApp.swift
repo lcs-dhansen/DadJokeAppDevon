487e434c97338b917a381eea1aa000639b5b591f
Devon Hanses <dhansen@lcs.on.ca>
2021-02-19 11:14:12 -0500

    Initial Commit

---
+++ DadJokeAppDevon/DadJokeAppDevonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokeAppDevonApp.swift
+//  DadJokeAppDevon
+//
+//  Created by Devon Hansen on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct DadJokeAppDevonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
